932060a435e5812213d0cd115682d5b34186e050
Arsen Jijavadze <arsen.jijavadze@gmail.com>
2023-01-20 21:50:51 +0300

    style: add basic page style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Block } from './Block';
 import './index.scss';
 
 function App() {
   return (
     <div className="App">
-
+      <Block value={0} currency="USD" onChangeCurrency={(cur) => console.log(cur)} />
+      <Block value={0} currency="GBP" />
     </div>
   );
 }
