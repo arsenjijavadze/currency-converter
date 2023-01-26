bc2bb332e27580e9b2ee5ebe0cf4f8fd282feb26
Arsen Jijavadze <arsen.jijavadze@gmail.com>
2023-01-26 21:13:38 +0300

    feat: implement request to exchangerate API

--- src/App.js
+++ src/App.js
@@ -2,11 +2,32 @@ import React from 'react';
 import { Block } from './Block';
 import './index.scss';
 
+const getCurrency = (from, to) => {
+  var requestURL = `https://api.exchangerate.host/convert?from=${from}&to=${to}`;
+  var request = new XMLHttpRequest();
+  request.open('GET', requestURL);
+  request.responseType = 'json';
+  request.send();
+
+  request.onload = function () {
+    var response = request.response;
+    console.log(response.result);
+  }
+}
+
+
+
 function App() {
+
+  const [rates, setRates] = React.useState({});
+
+  getCurrency('USD', 'EUR');
+
+
   return (
     <div className="App">
-      <Block value={0} currency="USD" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="GBP" />
+      <Block value={0} currency="USD" />
+      <Block value={0} currency="EUR" />
     </div>
   );
 }
