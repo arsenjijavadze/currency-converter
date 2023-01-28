ab057a4cfb157eb2e86377d7aa8474f3e35720bf
Arsen Jijavadze <arsen.jijavadze@gmail.com>
2023-01-28 16:49:45 +0300

    feat: implement convert feature

--- src/App.js
+++ src/App.js
@@ -2,32 +2,60 @@ import React from 'react';
 import { Block } from './Block';
 import './index.scss';
 
-const getCurrency = (from, to) => {
-  var requestURL = `https://api.exchangerate.host/convert?from=${from}&to=${to}`;
-  var request = new XMLHttpRequest();
-  request.open('GET', requestURL);
-  request.responseType = 'json';
-  request.send();
 
-  request.onload = function () {
-    var response = request.response;
-    console.log(response.result);
-  }
-}
 
 
 
 function App() {
 
   const [rates, setRates] = React.useState({});
+  const [from, setFrom] = React.useState('USD');
+  const [to, setTo] = React.useState('EUR');
+  const [fromPrice, setFromPrice] = React.useState(0);
+  const [toPrice, setToPrice] = React.useState(0);
 
-  getCurrency('USD', 'EUR');
+  // getCurrency('USD', 'EUR');
+
+  const onChangeFromPrice = (value) => {
+    setFromPrice(value);
+  }
+
+  const onChangeToPrice = (value) => {
+    setToPrice(value);
+  }
+
+  const getCurrency = (amount, from, to,) => {
+    var requestURL = `https://api.exchangerate.host/convert?from=${from}&to=${to}`;
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function () {
+      var response = request.response;
+      //console.log(response.result);
+      setToPrice(amount * response.result);
+      return response.result;
+    }
+  }
+
+  getCurrency(fromPrice, from, to);
 
 
   return (
     <div className="App">
-      <Block value={0} currency="USD" />
-      <Block value={0} currency="EUR" />
+      <Block
+        value={fromPrice}
+        currency={from}
+        onChangeCurrency={setFrom}
+        onChangeValue={onChangeFromPrice}
+      />
+      <Block
+        value={toPrice}
+        currency={to}
+        onChangeCurrency={setTo}
+        onChangeValue={getCurrency}
+      />
     </div>
   );
 }
