9c7ee758f4cd310e1a15f04b2a55612c9ed2cd36
Arsen Jijavadze <arsen.jijavadze@gmail.com>
2023-01-28 23:51:39 +0300

    refactor: remove non functional element in block

--- src/Block.jsx
+++ src/Block.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const defaultCurrencies = ['USD', 'EUR', 'GBP'];
+const defaultCurrencies = ['USD', 'EUR', 'GBP', 'GEL'];
 
 export const Block = ({ value, currency, onChangeValue, onChangeCurrency, type }) => (
     <div className="block">
@@ -13,12 +13,13 @@ export const Block = ({ value, currency, onChangeValue, onChangeCurrency, type }
                     {cur}
                 </li>
             ))}
-            <li>
+
+            {/* <li>
                 <svg heght="50px" viewBox="0 0 50 50" width="50px">
                     <rect fill="none" height="50" width="50" />
                     <polygon points="47.25,15 45.164,12.914 25,33.078 4.836,12.914 2.75,15 25,37.25 " />
                 </svg>
-            </li>
+            </li> */}
         </ul>
         <input
             inputMode="numeric"
